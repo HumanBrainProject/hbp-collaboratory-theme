1b1a05125f7a2febd7dcef616129d18a23947780
Olivier Amblet <olivier.amblet@epfl.ch>
2015-03-12 13:06:40 +0100

    noop Bootstrap 3 Theme
    Change-Id: Ibf794c11f6e2fcb734be45d84bd8529a15efab1f

---
+++ Gruntfile.js
@@ -0,0 +1,143 @@
+'use strict';
+module.exports = function (grunt) {
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
+
+  grunt.initConfig({
+    bowerDirectory: require('bower').config.directory,
+    pkg: grunt.file.readJSON('package.json'),
+
+
+    // -----------------------
+    // Build Tasks
+    // -----------
+
+    sass: {
+      options: {
+        sourceMap: true,
+        includePaths: ['sass', '<%= bowerDirectory %>/bootstrap-sass/assets/stylesheets']
+      },
+      dist: {
+        files: {
+            '.tmp/dist/css/bootstrap.css': 'sass/theme.scss'
+        }
+      }
+    },
+
+    cssmin: {
+      minify: {
+        expand: true,
+        cwd: '.tmp/dist/css',
+        src: ['*.css', '!*.min.css'],
+        dest: '.tmp/dist/css',
+        ext: '.min.css'
+      }
+    },
+
+    assemble: {
+      pages: {
+        options: {
+          data: './bower.json',
+          flatten: true,
+          assets: '.tmp/dist'
+        },
+        files: {
+          '.tmp/dist/index.html': ['pages/index.html'],
+          '.tmp/dist/examples/': ['pages/examples/*.html']
+        }
+      }
+    },
+
+    copy: {
+      ci: {
+        files: [
+          // all .tmp/dist directory
+          {expand: true, cwd: '.tmp/dist', src: ['**'], dest: 'dist/'},
+          // all sass file to dist/sass
+          {expand: true, src: ['sass/*'], dest: 'dist/'}
+        ]
+      }
+    },
+
+    clean: ['dist', '.tmp'],
+
+
+    // -----------------------
+    // Watch and Serve
+    // ---------------
+
+    watch: {
+      sass: {
+        files: ['sass/*.sass'],
+        tasks: ['sass:dist'],
+        options: {
+          livereload: true
+        }
+      },
+      cssmin: {
+        files: ['.tmp/dist/css/bootstrap.css'],
+        tasks: 'cssmin:minify'
+      },
+      assemble: {
+        files: ['pages/*.html', 'pages/examples/*', 'README.md'],
+        tasks: ['assemble']
+      }
+    },
+
+    connect: {
+      serve: {
+        options: {
+          port: grunt.option('port') || '8100',
+          hostname: grunt.option('host') || 'localhost',
+          base: '.tmp/dist'
+        }
+      }
+    },
+
+
+    // -----------------------
+    // Publish and Release
+    // ---------------
+
+    bump: {
+      options: {
+        files: ['package.json', 'bower.json'],
+        updateConfigs: ['pkg'],
+        commitFiles: ['package.json', 'bower.json', 'CHANGELOG.md'],
+        pushTo: 'origin HEAD:master'
+      }
+    },
+    changelog: {
+      options: {
+        commitLink: function(h) { return 'https://bbpteam.epfl.ch/reps/gerrit/platform/hbp/collaboratory-theme/commit/?id='+h; },
+        issueLink: function(issueId) { return 'https://bbpteam.epfl.ch/project/issues/browse/' + issueId; }
+      }
+    },
+    exec: {
+      npmTag: 'npm tag <%= pkg.name %>@<%= pkg.version %> latest'
+    }
+  });
+
+
+  // register task missed by load-grunt-task
+  grunt.loadNpmTasks('assemble');
+
+  // -----------------------
+  // Main Targets
+  // ------------
+
+  grunt.registerTask('ci', function (target) {
+    var tasks = ['default', 'copy:ci'];
+    if (target === 'patch' || target === 'minor' || target === 'major') {
+      tasks.unshift('bump-only:'+target);
+      tasks.push('changelog', 'bump-commit', 'exec:npmTag');
+    }
+    grunt.task.run(tasks);
+  });
+
+  grunt.registerTask('serve', ['connect', 'watch']);
+
+  grunt.registerTask('default', ['clean', 'sass', 'cssmin', 'assemble']);
+};
---
+++ README.md
@@ -0,0 +1,12 @@
+# collaboratory-theme
+
+A Bootstrap 3 Theme for HBP Collaboratory Extensions
+
+# Credits
+
+The project structure and build files are inspired by the [Themestrap] template.
+The kitchen sink especially is a direct copy of a huge amount of work from this
+project.
+The main variation is that we use Bootstrap Sass instead of Less.
+
+[Themestrap]: http://code.divshot.com/themestrap/
